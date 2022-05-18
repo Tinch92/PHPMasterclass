886e5583396ba3b5417f0c0769ec5a9fc7fbe434
Tina <martina.jalsevac@student.um.si>
2022-05-18 12:48:04 +0200

    Update indexOld.php

--- indexOld.php
+++ indexOld.php
@@ -4,7 +4,8 @@
  //$bar = [2,4,6,8];
  //array_push($bar, $foo);
  //print_r($bar);
-echo "Hello World!";
+echo "Hello World!<br>\n";
+echo "------------------------------------<br>";
 
 $fruits = array("Apple", "Orange" , "Lemon" , "Banana" , "Kiwi",  "Avocado", "Eldeberry");
 sort(array: $fruits);
@@ -14,11 +15,20 @@ $i=1;
 foreach($fruits as $fruit){
     echo "<p>Fruit nr. $i: $fruit <p>";
     $i++;
-}
+ }
 $fruits2 =["Apple"=>23, "Orange"=>52, "Lemon"=>4];
 
 $max=0;
 $nameMax="";
+
+echo "------------------------------------<br>";
+$ii = 1;
+foreach ($fruits as $value)
+{
+    if (in_array($value[0], ['A', 'E', 'I', 'O', 'U']))
+        echo 'Fruit nr.' . $ii . ': ' . $value . '<br><br>';
+        $ii = $ii + 1;
+}
 
 foreach($fruits2 as $name => $value)
 {
@@ -27,6 +37,5 @@ foreach($fruits2 as $name => $value)
         $nameMax=$name;
     }
 }
-
-echo "<p>----------<p>";
-echo "Max: " . $nameMax;
+echo "------------------------------------<br>";
+echo " Max: " . $nameMax;
